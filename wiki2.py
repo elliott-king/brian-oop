5623c30ca0ce0c99f6fca9e658682c57b778f3f5
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-11-13 13:01:40 -0500

    more refactoring

--- wiki2.py
+++ wiki2.py
@@ -34,45 +34,45 @@ def get_subtopics(list_item):
     
     return subtopics
 
+def create_list(ul):
+    list_topics = []
+    for list_item in ul.contents:
+        if list_item != "\n":
+
+            list_topics.append({
+                '_title': get_name(list_item),
+                'link': get_link(list_item),
+                'subtopics': get_subtopics(list_item),
+            })
+    return list_topics
+
+
 def get_design_dev_lists():
     url = "https://en.wikipedia.org/wiki/Outline_of_web_design_and_web_development"
     res = requests.get(url) # make a HTTP request for the page
     text = res.text # getting the actual HTML from the request
     soup = BeautifulSoup(text, 'html.parser')
 
-    list1 = []
     web_design = soup.find("span", id="Web_Design") # finding the web design section
     ul1 = web_design.parent.find_next_sibling("ul") # finding the unordered list of items in that section
-    for list_item in ul1.contents:
-        if list_item != "\n":
+    list1 = create_list(ul1)
 
-            list1.append({
-                '_title': get_name(list_item),
-                'link': get_link(list_item),
-                'subtopics': get_subtopics(list_item),
-            })
-
-    # list2 = []
-    # web_dev = soup.find("span", id="Web_Development")
-    # ul2 = web_dev.parent.next_sibling.next_sibling
-    # for list_item in ul2.contents:
-    #     if list_item != "\n":
-    #         name = get_name(list_item)
-    #         link = get_link(list_item)
-    #         subtopics = get_subtopics(list_item)
-    #         list2.append({
-    #             '_title': name,
-    #             'link': link,
-    #             'subtopics': subtopics,
-    #         })
+    web_dev = soup.find("span", id="Web_Development")
+    ul2 = web_dev.parent.next_sibling.next_sibling
+    list2 = create_list(ul2)
 
     file = open("output2.txt", "w+")
     pprint.pprint("Web Design List:", file)
     pprint.pprint(list1, file)
     pprint.pprint("", file)
+    pprint.pprint("", file)
+    pprint.pprint("", file)
+    pprint.pprint("", file)
+    pprint.pprint("", file)
+    pprint.pprint("", file)
     # pprint.pprint("Web Development List: ", list2)
-    # pprint.pprint("Web Development List:", file)
-    # pprint.pprint(list2, file)
+    pprint.pprint("Web Development List:", file)
+    pprint.pprint(list2, file)
     file.close()
 
 if __name__ == "__main__":
